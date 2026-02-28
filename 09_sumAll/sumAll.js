ab81301c3efa03bd2f50253e6506afa33faa1535
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-02-28 12:57:01 +0300

    JS loops and Arrays and logic Exercises
    Here, Complteted loops and arrays and some logic exercises with a focus
    on Test Driven Development

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    //Validation must always happen first before doing any math to avoid
+    //type coercion by JS
+    if (Number.isNaN(a) || Number.isNaN(b)){
+        return 'ERROR';
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    } else if ( a < 0 || b < 0){
+        return 'ERROR';
+    };
+
+    let smallInteger = Math.min(a, b);
+    let largeInteger = Math.max(a, b);
+
+    let total = 0;
+
+    for (let i = smallInteger; i <= largeInteger; i++){
+        total += i;
+    }
+
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

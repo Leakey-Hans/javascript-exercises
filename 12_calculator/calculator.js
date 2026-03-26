584c89dde5d99d44a87fdb31b2e4aa5481d58a77
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-03-26 14:18:15 +0300

    JavaScript Exercises
    Completed JS Exercises array methods, functions, objects

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+/*When you you you use parenthesis in an arrow function you must 
+return inside it but i went with a shorter version that return automatically
+by removing the parenthesis */
+const sum = function(array) {
+	return array.reduce((acc, currentVal) => acc + currentVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.reduce((acc, currentVal) => acc * currentVal);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
